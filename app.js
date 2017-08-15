027fbb00de3aa6d4d61fc0c12fd4c57e542a678c
sam <wade@heckledesign.com>
2017-08-15 08:53:16 -0600

    Adding files and testing

--- app.js
+++ app.js
@@ -3,20 +3,25 @@ var app = express();
 
 var port = 3000;
 
-app.get('/',function(req,res) {
- res.send('We rule');
+
+app.get('/', function(req, res) {
+  res.send('Heckle Me');
 
 });
+
+app.get('/routing', function(req, res) {
+  res.send('Heckle Tomorrow');
+
+});
+
+
+app.use(express.static('public'));
+app.use(express.static('src/views'));
+app.use(express.static('node_modules'));
+
 
 
 app.listen(port,function(err) {
   console.log('The server is running on port:' + port);
 
 });
-
-
-
-var aloha = 'Heckle Designing Baby!';
-console.log(aloha);
-
-console.log('What\'s Up!');
--- server.js
+++ server.js
@@ -3,7 +3,33 @@ var app = express();
 
 var port = 3000;
 
+
+server.get('/', function(req, res) {
+  res.send('Heckle Me');
+
+});
+
 app.listen(port,function(err) {
   console.log('The server is running on port:' + port);
+
+});
+
+// server model 
+
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 4000;
+
+const server = http.createServer((req,res) => {  
+ res.StatusCode = 200;
+ res.setHeader('Content-Type', 'text/plain');
+ res.end('Heckle Me Silly !\n');
+
+});
+
+
+server.listen(port, hostname, () => {
+  console.log('Server running at http://${hostname}:${port}/');
 
 });
